d2faf200a8f7faa307f47be9e82d1826616760bd
Sreeram Narayanan <sreeram.an1994@gmail.com>
2024-11-06 22:02:45 +0530

    add: Money with symbol in Ticket model

--- models/ticket.go
+++ models/ticket.go
@@ -1,16 +1,32 @@
 package models
 
+import "fmt"
+
 type Ticket struct {
 	ID      string
-	UserID  User
+	UserID  string
 	From    string
 	To      string
 	Number  string
 	Section string
 	Price   Money
+	User    User
 }
 
 type Money struct {
-	Amount   float64
-	Currency string
+	amount   float64
+	currency string
+	symbol   string
+}
+
+func NewMoney(amount float64) Money {
+	return Money{
+		amount:   amount,
+		currency: "USD",
+		symbol:   "$",
+	}
+}
+
+func (m Money) String() string {
+	return fmt.Sprintf("%f %s", m.amount, m.symbol)
 }
