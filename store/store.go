8982a8b4fdf00cce6d059ac993793e36a397800e
Sreeram Narayanan <sreeram.an1994@gmail.com>
2024-11-06 18:09:14 +0530

    add: store interfaces

---
+++ store/store.go
@@ -0,0 +1,20 @@
+package store
+
+import "github.com/sreeram77/ticketor/models"
+
+type Users interface {
+	Create(user models.User) (models.User, error)
+	Get(id string) (models.User, error)
+	Remove(id string) error
+}
+
+type Tickets interface {
+	Create(ticket models.Ticket) (models.Ticket, error)
+	Get(id string) (models.Ticket, error)
+	Remove(id string) error
+	Modify(id string, ticket models.Ticket) (models.Ticket, error)
+}
+
+type Section interface {
+	Layout(id string) error
+}
